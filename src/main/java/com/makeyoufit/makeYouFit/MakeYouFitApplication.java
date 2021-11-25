6620caa8cc1e2f542cc87e1a5566ad1f50659978
Pulkit Singh <pulkitsingh@Pulkits-MacBook-Pro.local>
2021-11-25 10:55:27 +0530

    withchanges

---
+++ src/main/java/com/makeyoufit/makeYouFit/MakeYouFitApplication.java
@@ -0,0 +1,14 @@
+package com.makeyoufit.makeYouFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeYouFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeYouFitApplication.class, args);
+		System.out.println("Hello Pulkit!");
+	}
+
+}
---
+++ src/test/java/com/makeyoufit/makeYouFit/MakeYouFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeyoufit.makeYouFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeYouFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
